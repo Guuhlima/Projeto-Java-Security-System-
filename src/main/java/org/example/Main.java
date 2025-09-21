e830cdb7dadfc79f72ee9a0852f1e28f7fa53498
Gustavo Alves Fonseca De Lima <guuhalves0922@gmail.com>
2025-09-21 11:52:16 -0300

    Atualizando brnach

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class Main {
-    public class ProjectSecuritySystem {
+    public static class ProjectSecuritySystem {
         public static void main(String[] args) {
             SpringApplication.run(ProjectSecuritySystem.class, args);
         }
