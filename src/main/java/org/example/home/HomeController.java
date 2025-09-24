26db0dfec3f1bb07614be372bbf5ab3bef747127
Gustavo Alves Fonseca De Lima <guuhalves0922@gmail.com>
2025-09-23 22:34:26 -0300

    feat create register

--- src/main/java/org/example/home/HomeController.java
+++ src/main/java/org/example/home/HomeController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
-    @GetMapping("/") // <- homepage pública
-    public String index(Model model) {
-        model.addAttribute("mensagem", "Spring Boot + Thymeleaf + MongoDB Atlas funcionando!");
+
+    @GetMapping("/")
+    public String home(Model model, Principal principal) {
+        model.addAttribute("mensagem", "Você está logado! Bem-vindo à dashboard.");
+        if (principal != null) {
+            model.addAttribute("user", principal.getName()); // normalmente o e-mail/username
+        }
         return "index";
     }
 }
